9cb7e4d5a8ba63bc661a6dc792478df61519532d
Raviteja119 <ravitejathripuraribhotla99@gmail.com>
2026-06-13 12:02:03 +0530

    Update DashboardRoutes.js

--- Backend/routes/DashboardRoutes.js
+++ Backend/routes/DashboardRoutes.js
@@ -2,13 +2,13 @@ const express = require("express");
 
 const router = express.Router();
 
-const auth = require("../middleware/authMiddleware");
-const role = require("../middleware/roleMiddleware");
+const auth = require("../middleware/AuthMiddleware");
+const role = require("../middleware/RoleMiddleware");
 
 const {
   getAdminDashboard,
   getUserDashboard
-} = require("../controllers/dashboardController");
+} = require("../controllers/DashboardController");
 
 router.get("/me", auth, getUserDashboard);
 router.get("/", auth, role("admin"), getAdminDashboard);
