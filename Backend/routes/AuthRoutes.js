e72e2f09c719076307193f5450d94a4deb1dd566
Raviteja119 <ravitejathripuraribhotla99@gmail.com>
2026-06-13 11:58:36 +0530

    Update AuthRoutes.js

--- Backend/routes/AuthRoutes.js
+++ Backend/routes/AuthRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const {
   register,
   login
-} = require("../controllers/authController");
+} = require("../controllers/AuthController");
 
 router.post("/register",register);
 
