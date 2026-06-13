810d3fea00772c90fe286eb7385d812c6181be2a
Raviteja119 <ravitejathripuraribhotla99@gmail.com>
2026-06-13 12:22:40 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://smart-student-learning-km8m.onrender.com/api",
 });
 
 // Attach token if present
